f0221bad9fd8028aac3b69d9992a38f70e9482ba
aditipandey16 <101034485+aditipandey16@users.noreply.github.com>
2022-04-01 18:08:05 +0530

    Add files via upload

---
+++ practiceset2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    if(a%97==0)
+    printf("True");
+   
+    else
+    printf("False");
+    
+}
