53c29ee3576c4fffce179404427067ee62af440b
aditipandey16 <101034485+aditipandey16@users.noreply.github.com>
2022-04-01 18:06:41 +0530

    Add files via upload

---
+++ practiceset1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int l,b,res;
+    printf("Enter the breadth:");
+    scanf("%d",&b);
+    printf("enter the length:");
+    scanf("%d",&l);
+    res=l*b;
+    printf("%d",res);
+
+}
